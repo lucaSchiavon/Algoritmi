b2c8a9351ff3c18fbb58ff939037c181d4c5b4f6
lucaSchiavon <luca.schiavon@alice.it>
2021-05-07 12:00:35 +0200

    primo commit

---
+++ Algoritmi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algoritmi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
